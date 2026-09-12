9140a9b4d9583609ac1ff4415deb8062913328ee
Nayan Das <das680970@gmail.com>
2026-09-13 02:51:12 +0600

    first check-2:48

--- answer.js
+++ answer.js
@@ -89,5 +89,14 @@ const getChaseVerdict = (target, scored, ballsLeft) => {
   }
 
   return  `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+  console.log(object);
+  
 
   };
+
+
+
+  //hello this i s check 1
+  //hello this i s check 1
+  // /hello this i s check 1
+  // /hello this i s check 1
